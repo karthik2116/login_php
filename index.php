5114d7fd509608eb1049c79275a8085ce8967efb
karthik2116 <58129803+karthik2116@users.noreply.github.com>
2020-03-19 07:43:46 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<form action="b.php" method="post">
+Name: <input type="text" name="name"><br>
+Password: <input type="text" name="pass"><br>
+<input type="submit">
+</form>
+</body>
+</html>
